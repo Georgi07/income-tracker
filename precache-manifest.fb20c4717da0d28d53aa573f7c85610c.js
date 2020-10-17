a3bb83d60391014413327c18d19ba13abe930a66
unknown <g.nikolaev@abv.bg>
2020-10-17 17:58:08 +0300

    Updates

---
+++ precache-manifest.fb20c4717da0d28d53aa573f7c85610c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f93a74cf3e738e712d86a198506edd",
+    "url": "/income-tracker/index.html"
+  },
+  {
+    "revision": "0e4442577a2dacab1514",
+    "url": "/income-tracker/static/css/main.ac010ca8.chunk.css"
+  },
+  {
+    "revision": "48bbc31aa36de50fc26e",
+    "url": "/income-tracker/static/js/2.f2baaed5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/income-tracker/static/js/2.f2baaed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4442577a2dacab1514",
+    "url": "/income-tracker/static/js/main.9e1df95d.chunk.js"
+  },
+  {
+    "revision": "d16eadb4a4ebda47f49f",
+    "url": "/income-tracker/static/js/runtime-main.00de9406.js"
+  }
+]);
